321e8e9cda7d39224a4ddd9fb67ae0364369573d
daratayver <daratayver@gmail.com>
2021-09-03 23:46:54 +0700

    лабы

---
+++ 13.StatisticsOfDiscreteRandomVariableSimulation/StatisticsOfDiscreteRandomVariableSimulation/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StatisticsOfDiscreteRandomVariableSimulation
+{
+	public partial class Form1 : Form
+	{
+		readonly Processing processing;
+		public Form1()
+		{
+			InitializeComponent();
+			processing = new Processing();
+		}
+
+		private void ProbValue_ValueChanged(object sender, EventArgs e)
+		{
+			var prob5 = 1 - (prob1Value.Value + prob2Value.Value + prob3Value.Value + prob4Value.Value);
+			if (prob5 < 0)
+			{
+				prob5Value.Text = "ERROR";
+				prob5Value.BackColor = Color.Red;
+				startButton.Enabled = false;
+			}
+			else
+			{
+				prob5Value.Text = prob5.ToString("f3");
+				prob5Value.BackColor = Color.White;
+				startButton.Enabled = true;
+			}
+		}
+
+		private void StartButton_Click(object sender, EventArgs e)
+		{
+			int size = 5;
+			decimal[] probs = new decimal[]
+			{
+				prob1Value.Value,
+				prob2Value.Value,
+				prob3Value.Value,
+				prob4Value.Value,
+				decimal.Parse(prob5Value.Text)
+			};
+
+			processing.GetStatistics(probs, size, (int)numberValue.Value);
+			var frequency = processing.Probabilities;
+
+			chart1.Series[0].Points.Clear();
+			for (int i = 0; i < size; i++)
+			{
+				chart1.Series[0].Points.AddXY(i + 1, frequency[i]);
+			}
+
+			average.Text = processing.Average.ToString("F3") + " (error = " + decimal.Round(processing.AverageError * 100) + "%)";
+			variance.Text = processing.Variance.ToString("F3") + " (error = " + decimal.Round(processing.VarianceError * 100) + "%)";
+
+			chiSquared.Text = processing.ChiSquare.ToString("F3") + " <= " + processing.CriticalValue.ToString("F3") + " is ";
+			
+			if (processing.ChiSquareTest)
+			{
+				test.Text = "true";
+				test.ForeColor = Color.Green;
+			}
+			else
+			{
+				test.Text = "false";
+				test.ForeColor = Color.Red;
+			}
+		}
+	}
+}
---
+++ 13.StatisticsOfDiscreteRandomVariableSimulation/StatisticsOfDiscreteRandomVariableSimulation/Processing.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatisticsOfDiscreteRandomVariableSimulation
+{
+	class Processing
+	{
+		readonly Random random = new Random();
+		public decimal CriticalValue { get => 9.488M; }
+
+		public decimal Average { get; private set; }
+		public decimal AverageError { get; private set; }
+		public decimal Variance { get; private set; }
+		public decimal VarianceError { get; private set; }
+
+		public decimal ChiSquare { get; private set; }
+		public bool ChiSquareTest { get; private set; }
+
+		public decimal[] Probabilities { get; private set; }
+
+		public void GetStatistics(decimal[] probs, int size, int N)
+		{
+			decimal[] statistics = new decimal[size];
+			Probabilities = new decimal[size];
+			for (int i = 0; i < N; i++)
+			{
+				var alpha = (decimal)random.NextDouble();
+
+				for (int j = 0; j < size; j++)
+				{
+					alpha -= probs[j];
+
+					if (alpha <= 0)
+					{
+						statistics[j]++;
+						break;
+					}
+				}
+			}
+
+			for (int i = 0; i < size; i++)
+			{
+				Probabilities[i] = statistics[i] / N;
+			}
+
+			Average = 0;
+			decimal E = 0;
+			for (int i = 0; i < Probabilities.Length; i++)
+			{
+				var x = i + 1;
+				Average += Probabilities[i] * x;
+				E += probs[i] * x;
+			}
+			AverageError = Math.Abs((Average - E) / E);
+
+			Variance = 0;
+			decimal D = 0;
+			for (int i = 0; i < Probabilities.Length; i++)
+			{
+				var x = i + 1;
+				Variance += Probabilities[i] * x * x;
+				D += probs[i] * x * x;
+			}
+			Variance -= Average * Average;
+			D -= E * E;
+			VarianceError = Math.Abs((Variance - D) / D);
+
+			ChiSquare = 0;
+			for (int i = 0; i < Probabilities.Length; i++)
+			{
+				ChiSquare += statistics[i] * statistics[i] / (N * probs[i]);
+			}
+			ChiSquare -= N;
+
+			if (ChiSquare <= CriticalValue) ChiSquareTest = true;
+			else ChiSquareTest = false;
+		}
+	}
+}
